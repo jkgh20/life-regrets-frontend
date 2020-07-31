85d0965b17a49388f6cfd9ee5bc35280f1a06e22
jkgh20 <jkgh20@users.noreply.github.com>
2020-07-31 18:35:48 -0400

    Finish About text

--- src/About.js
+++ src/About.js
@@ -11,7 +11,38 @@ function About(props) {
       </Modal.Header>
 
       <Modal.Body>
-        Marry, and you will regret it; don’t marry, you will also regret it; marry or don’t marry, you will regret it either way. Laugh at the world’s foolishness, you will regret it; weep over it, you will regret that too; laugh at the world’s foolishness or weep over it, you will regret both. Believe a woman, you will regret it; believe her not, you will also regret it… Hang yourself, you will regret it; do not hang yourself, and you will regret that too; hang yourself or don’t hang yourself, you’ll regret it either way; whether you hang yourself or do not hang yourself, you will regret both. This, gentlemen, is the essence of all philosophy.
+<span className="regret-word">Regrets</span> are a funny thing. They often paint
+a picture of one's wants and desires moreso than asking that person directly.
+As one might come to read, some are whimsical while others show a deep longing
+for a reality that isn't.
+<br />
+<br />
+<span className="regret-word">Regrets</span> often bring pain. Most of us have
+something we'd like to change about our lives. Some wish they didn't have this
+life at all. I believe the gnawing sense of having had and lost some infinite
+thing starts in adolescence, finds its peaks during adulthood, and finally
+quiets down when we not only know but deeply understand that life just
+<em>is</em>.
+<br />
+<br />
+Chances are, you the reader are living along these peaks. The point of this
+journal isn't to ruminate on these feelings, but rather to empathize, vent, let
+go, and learn from each other. Truly learn to forgive yourself if you haven't
+yet - you didn't know what you know now.
+<br />
+<br />
+In my own journey, I myself have come to believe that my feelings of
+<span className="regret-word">regret</span> stem not from my circumstances, but
+from who I am as a person - and I'm incredibly lucky to be able to say that. I'm
+realizing that even if I were given a chance to start over, I would STILL have
+<span className="regret-word">regrets</span> and want to do it over again. Call
+this a personal moment of catharsis if you will.
+<br />
+<br />
+I do hope you'll walk away with <em>something</em> after reading a couple of
+these entries. Some of the biggest moments of clarity can come from the most
+mundane moments. If you get the chance, call your family and give your ma a big
+hug.
       </Modal.Body>
     </Modal>
   );
