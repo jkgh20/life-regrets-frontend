1590f514b57ba9aedb54a36f5d048dbe29bec149
jkgh20 <jkgh20@users.noreply.github.com>
2020-07-31 18:57:04 -0400

    Fix html

--- src/About.js
+++ src/About.js
@@ -11,18 +11,16 @@ function About(props) {
       </Modal.Header>
 
       <Modal.Body>
-<span className="regret-word">Regrets</span> are a funny thing. They often paint
-a picture of one's wants and desires moreso than asking that person directly.
-As one might come to read, some are whimsical while others show a deep longing
-for a reality that isn't.
+Regrets are a funny thing. They often paint a picture of one's wants and desires
+moreso than asking that person directly. As one might come to read, some are
+whimsical while others show a deep longing for a reality that isn't.
 <br />
 <br />
-<span className="regret-word">Regrets</span> often bring pain. Most of us have
-something we'd like to change about our lives. Some wish they didn't have this
-life at all. I believe the gnawing sense of having had and lost some infinite
-thing starts in adolescence, finds its peaks during adulthood, and finally
-quiets down when we not only know but deeply understand that life just
-<em>is</em>.
+Regrets often bring pain. Most of us have something we'd like to change about
+our lives. Some wish they didn't have this life at all. I believe the gnawing
+sense of having had and lost some infinite thing starts in adolescence, finds
+its peaks during adulthood, and finally quiets down when we not only know but
+deeply understand that life just <em>is</em>.
 <br />
 <br />
 Chances are, you the reader are living along these peaks. The point of this
@@ -32,11 +30,10 @@ yet - you didn't know what you know now.
 <br />
 <br />
 In my own journey, I myself have come to believe that my feelings of
-<span className="regret-word">regret</span> stem not from my circumstances, but
-from who I am as a person - and I'm incredibly lucky to be able to say that. I'm
-realizing that even if I were given a chance to start over, I would STILL have
-<span className="regret-word">regrets</span> and want to do it over again. Call
-this a personal moment of catharsis if you will.
+regret stem not from my circumstances, but from who I am as a person - and I'm
+incredibly lucky to be able to say that. I'm realizing that even if I were given
+a chance to start over, I would STILL have regrets and want to do it over again.
+Call this a personal moment of catharsis if you will.
 <br />
 <br />
 I do hope you'll walk away with <em>something</em> after reading a couple of
