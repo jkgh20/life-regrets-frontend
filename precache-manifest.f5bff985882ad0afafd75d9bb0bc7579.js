2ce866686a732c1b8e367f6d180d6df4c9f66b50
jkgh20 <jkgh20@users.noreply.github.com>
2020-08-01 07:41:40 -0400

    Updates

---
+++ precache-manifest.f5bff985882ad0afafd75d9bb0bc7579.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfb1e2df44543adbfeaf8731eccf5fbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed1c6424947ad88a43c0",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ecae3c1c631c5ffb278f",
+    "url": "/static/css/main.05cd1aa1.chunk.css"
+  },
+  {
+    "revision": "ed1c6424947ad88a43c0",
+    "url": "/static/js/2.6f6ce4fa.chunk.js"
+  },
+  {
+    "revision": "74c519d158df4771dcdf3b54fb80c969",
+    "url": "/static/js/2.6f6ce4fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ecae3c1c631c5ffb278f",
+    "url": "/static/js/main.ba67f5ea.chunk.js"
+  },
+  {
+    "revision": "3ac07a769daeef7f231c",
+    "url": "/static/js/runtime-main.5a22bdfa.js"
+  }
+]);
